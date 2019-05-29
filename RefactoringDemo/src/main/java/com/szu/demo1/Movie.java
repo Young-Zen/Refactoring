126a35a5c329134a8bb740288dc492da46a2cf47
yanghaojia <2453883990@qq.com>
2019-05-29 19:40:12 +0800

    起始程序

---
+++ RefactoringDemo/src/main/java/com/szu/demo1/Movie.java
@@ -0,0 +1,38 @@
+package com.szu.demo1;
+
+/**
+ * @Author: YHJ
+ * @Description:
+ * @Date:Create：in 5/29/2019 15:33
+ * @Modified By：
+ */
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
+    }
+}
---
+++ RefactoringDemo/src/main/java/com/szu/demo1/Rental.java
@@ -0,0 +1,33 @@
+package com.szu.demo1;
+
+/**
+ * @Author: YHJ
+ * @Description:
+ * @Date:Create：in 5/29/2019 15:49
+ * @Modified By：
+ */
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public void setDaysRented(int daysRented) {
+        this.daysRented = daysRented;
+    }
+}
